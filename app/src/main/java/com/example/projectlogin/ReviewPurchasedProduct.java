d5a5f9a980589f3a3548536e3dc4f85138a3d36c
kurozemi <tranngminhdao@gmail.com>
2020-10-27 21:28:29 +0700

    onListener RatingBar

--- app/src/main/java/com/example/projectlogin/ReviewPurchasedProduct.java
+++ app/src/main/java/com/example/projectlogin/ReviewPurchasedProduct.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -19,6 +20,10 @@ public class ReviewPurchasedProduct extends AppCompatActivity {
 
     private ImageView product_pic;
     private TextView product_name;
+
+    private RatingBar ratingBar;
+    private TextView ratingText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,17 +31,42 @@ public class ReviewPurchasedProduct extends AppCompatActivity {
 
         product_pic = findViewById(R.id.product_pic);
         product_name = findViewById(R.id.product_name);
+        ratingBar = findViewById(R.id.ratingBar);
+        ratingText = findViewById(R.id.rating_text);
 
         final String p_name = getIntent().getExtras().getString("P_Name");
         product_name.setText(p_name);
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                int rate = (int) v;
+                switch (rate) {
+                    case 1:
+                        ratingText.setText("Awful");
+                        break;
+                    case 2:
+                        ratingText.setText("Bad");
+                        break;
+                    case 3:
+                        ratingText.setText("Not Good");
+                        break;
+                    case 4:
+                        ratingText.setText("Very Good");
+                        break;
+                    case 5:
+                        ratingText.setText("Perfect");
+                        break;
+                }
+            }
+        });
 
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Products");
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 for (DataSnapshot dataSnapshot : snapshot.getChildren())
-                    for (DataSnapshot _data: dataSnapshot.getChildren())
-                    {
+                    for (DataSnapshot _data : dataSnapshot.getChildren()) {
                         if (_data.child("name").getValue().equals(p_name)) {
                             Glide.with(ReviewPurchasedProduct.this).load(_data.child("avatarURL").getValue()).into(product_pic);
                             break;

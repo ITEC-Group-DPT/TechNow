3f70f29fed3a446f957c9ef74cabe5f157e6d8ee
kurozemi <tranngminhdao@gmail.com>
2020-11-04 11:07:35 +0700

    Remember Address

--- app/src/main/java/com/example/projectlogin/PaymentActivity.java
+++ app/src/main/java/com/example/projectlogin/PaymentActivity.java
@@ -7,12 +7,15 @@ import android.content.res.ColorStateList;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.method.SingleLineTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -47,8 +50,14 @@ public class PaymentActivity extends AppCompatActivity implements ChangeAddressF
     private NumberFormat format = new DecimalFormat("#,###");
     private String formattedTotalCash;
     private int Cart_totalcash;
+    private DatabaseReference address_book = DatabaseRef.getDatabaseReference().child("Address book");;
 
     private double shippingFee;
+
+    TextView cus_namephone_tv;
+    TextView cus_address_tv;
+    TextView tv_shipping;
+
     private String cus_name = "";
     private String cus_phone = "";
     private String cus_address = "";
@@ -61,84 +70,134 @@ public class PaymentActivity extends AppCompatActivity implements ChangeAddressF
         cus_phone = phone;
         cus_address = address;
 
-        TextView cus_namephone_tv = findViewById(R.id.customer_namephonenum);
-        TextView cus_address_tv = findViewById(R.id.customer_address);
-
         cus_namephone_tv.setText(name + " - " + phone);
         cus_address_tv.setText(address);
 
-        double distance = Calc_Distance(address);
-                shippingFee = distance * 2;
-                shippingFee = Math.round(shippingFee / 100) * 100;
-                if (shippingFee < 10000) shippingFee = 0;
-                if (shippingFee > 50000) shippingFee = 50000;
+        tv_shipping.setText(format.format(Calc_ShippingFee(address)) + "₫");
 
-                TextView tv_shipping = findViewById(R.id.shipping_fee_payment);
+        tv_total.setText(format.format(shippingFee + Cart_totalcash) + "₫");
 
-                tv_shipping.setText(format.format(shippingFee) + "₫");
-
-
-                tv_total = findViewById(R.id.total_cash);
-                tv_total.setText(format.format(shippingFee + Cart_totalcash) + "₫");
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
-        DatabaseRef.getDatabaseReference().child("Cart").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()) {
-                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                        Product product = dataSnapshot.getValue(Product.class);
-                        cart.addItem(product);
-                    }
-                }
-                Cart_totalcash = cart.calTotalCash();
-                formattedTotalCash = format.format(Cart_totalcash) + "₫";
-                TextView notional_price = findViewById(R.id.notional_price_payment);
-                notional_price.setText(formattedTotalCash);
-                packageDetailonCreate();
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-            }
-        });
+        packageDetailonCreate();
     }
 
-    private void packageDetailonCreate() {
+    private class AsyncTaskLoadPayment extends AsyncTask<String, String, String>
+    {
 
-        RadioButton radioButton = findViewById(R.id.radio_button);
-        radioButton.setChecked(true);
-        LinearLayout linearLayout = findViewById(R.id.package_detail);
-        for (int i = 0; i < cart.getCartArrList().size(); i++) {
-            View view = LayoutInflater.from(this).inflate(R.layout.product_lnlo_orderdetail, null, false);
-            ImageView product_pic = view.findViewById(R.id.order_pic);
-            TextView product_name = view.findViewById(R.id.name_product);
-            TextView product_price_qty = view.findViewById(R.id.price_qty);
+        @Override
+        protected String doInBackground(String... strings) {
 
-            final Product product = cart.getCartArrList().get(i);
-
-            Glide.with(getApplicationContext()).load(product.getAvatarURL()).into(product_pic);
-            product_name.setText(product.getName());
-
-            NumberFormat format = new DecimalFormat("#,###");
-            String formattedPrice = format.format(product.getPrice()) + " ₫";
-            product_price_qty.setText(formattedPrice + " x" + product.getQuantity());
-
-            view.setOnClickListener(new View.OnClickListener() {
+            DatabaseRef.getDatabaseReference().child("Cart").addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(PaymentActivity.this, ItemDetail.class);
-                    intent.putExtra("itemName", product.getName());
-                    intent.putExtra("itemType", product.getType());
-                    startActivity(intent);
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                            Product product = dataSnapshot.getValue(Product.class);
+                            cart.addItem(product);
+                        }
+                    }
+                    Cart_totalcash = cart.calTotalCash();
+                    formattedTotalCash = format.format(Cart_totalcash) + "₫";
+
+                    onPostExecute("Completed");
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
                 }
             });
-            linearLayout.addView(view);
+
+
+            return "";
         }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            ProgressBar progressBar = findViewById(R.id.progress_bar_payment);
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(String command) {
+            super.onPostExecute(command);
+            if (command.equals("Completed"))
+            {
+                TextView notional_price = findViewById(R.id.notional_price_payment);
+                notional_price.setText(formattedTotalCash);
+
+                tv_total.setText(formattedTotalCash);
+
+                LinearLayout linearLayout = findViewById(R.id.package_detail);
+                for (int i = 0; i < cart.getCartArrList().size(); i++) {
+                    View view = LayoutInflater.from(PaymentActivity.this).inflate(R.layout.product_lnlo_orderdetail, null, false);
+                    ImageView product_pic = view.findViewById(R.id.order_pic);
+                    TextView product_name = view.findViewById(R.id.name_product);
+                    TextView product_price_qty = view.findViewById(R.id.price_qty);
+
+                    final Product product = cart.getCartArrList().get(i);
+
+                    Glide.with(getApplicationContext()).load(product.getAvatarURL()).into(product_pic);
+                    product_name.setText(product.getName());
+
+                    NumberFormat format = new DecimalFormat("#,###");
+                    String formattedPrice = format.format(product.getPrice()) + " ₫";
+                    product_price_qty.setText(formattedPrice + " x" + product.getQuantity());
+
+                    view.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Intent intent = new Intent(PaymentActivity.this, ItemDetail.class);
+                            intent.putExtra("itemName", product.getName());
+                            intent.putExtra("itemType", product.getType());
+                            startActivity(intent);
+                        }
+                    });
+                    linearLayout.addView(view);
+                }
+            }
+            address_book.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    DataSnapshot dataSnapshot = snapshot.child("Address - 01");
+                    if (dataSnapshot.exists())
+                    {
+                        cus_name = (String) dataSnapshot.child("Name").getValue();
+                        cus_phone = (String) dataSnapshot.child("Phone Number").getValue();
+                        cus_address = (String) dataSnapshot.child("Address").getValue();
+
+                        cus_namephone_tv.setText(cus_name + " - " + cus_phone);
+                        cus_address_tv.setText(cus_address);
+                        tv_shipping.setText(format.format(Calc_ShippingFee(cus_address)) + "₫");
+                    }
+
+                    ProgressBar progressBar = findViewById(R.id.progress_bar_payment);
+                    progressBar.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+    }
+    private void packageDetailonCreate() {
+
+        tv_total = findViewById(R.id.total_cash);
+        tv_shipping = findViewById(R.id.shipping_fee_payment);
+        cus_namephone_tv = findViewById(R.id.customer_namephonenum);
+        cus_address_tv = findViewById(R.id.customer_address);
+        RadioButton radioButton = findViewById(R.id.radio_button);
+        radioButton.setChecked(true);
+
+        new AsyncTaskLoadPayment().execute("");
     }
 
 
@@ -179,6 +238,28 @@ public class PaymentActivity extends AppCompatActivity implements ChangeAddressF
                         public void onCancelled(@NonNull DatabaseError error) {
                         }
                     });
+
+                    address_book.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            for (DataSnapshot dataSnapshot : snapshot.getChildren())
+                            {
+                                if (dataSnapshot.child("Address").getValue().equals(cus_address))
+                                    return;
+                            }
+                            DatabaseReference book = address_book.child("Address - 0" + ((int) snapshot.getChildrenCount()  +1));
+                            book.child("Name").setValue(cus_name);
+                            book.child("Address").setValue(cus_address);
+                            book.child("Phone Number").setValue(cus_phone);
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+
                     DatabaseReference reff = FirebaseDatabase.getInstance().getReference("Products");
                     for (int i = 0; i < cart.getNoOfItem(); i++) {
                         final String type = cart.getCartArrList().get(i).getType();
@@ -245,7 +326,7 @@ public class PaymentActivity extends AppCompatActivity implements ChangeAddressF
             frameLayout.setVisibility(View.GONE);
     }
 
-    private double Calc_Distance(String location) {
+    private double Calc_ShippingFee(String location) {
         //latlng cho dia chi nguoi dung
         List<Address> addressList = null;
         LatLng latLng1 = null, latLng2;
@@ -279,7 +360,14 @@ public class PaymentActivity extends AppCompatActivity implements ChangeAddressF
         float[] results = new float[1];
         Location.distanceBetween(latLng1.latitude, latLng1.longitude, latLng2.latitude, latLng2.longitude, results);
         double res = results[0];
-        return res;
+
+        shippingFee = res * 2;
+        shippingFee = Math.round(shippingFee / 100) * 100;
+        if (shippingFee < 10000) shippingFee = 0;
+        if (shippingFee > 50000) shippingFee = 50000;
+
+        return shippingFee;
+
     }
 
     public void change_customer_detail(View view) {

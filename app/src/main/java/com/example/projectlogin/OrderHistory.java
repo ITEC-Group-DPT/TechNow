d39d4e36ec89b8f6445e9db4471627a59c61b2ba
kurozemi <tranngminhdao@gmail.com>
2020-11-08 20:14:31 +0700

    linh tinh

--- app/src/main/java/com/example/projectlogin/OrderHistory.java
+++ app/src/main/java/com/example/projectlogin/OrderHistory.java
@@ -5,6 +5,42 @@ import java.util.ArrayList;
 public class OrderHistory {
     private ArrayList<Cart> orderHistory = new ArrayList<>();
     private String ID;
+    private String cus_name;
+    private String cus_phonenum;
+    private String cus_address;
+    private String datetime;
+
+    public String getCus_name() {
+        return cus_name;
+    }
+
+    public void setCus_name(String cus_name) {
+        this.cus_name = cus_name;
+    }
+
+    public String getCus_phonenum() {
+        return cus_phonenum;
+    }
+
+    public void setCus_phonenum(String cus_phonenum) {
+        this.cus_phonenum = cus_phonenum;
+    }
+
+    public String getCus_address() {
+        return cus_address;
+    }
+
+    public void setCus_address(String cus_address) {
+        this.cus_address = cus_address;
+    }
+
+    public String getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(String datetime) {
+        this.datetime = datetime;
+    }
 
     public String getID() {
         return ID;
